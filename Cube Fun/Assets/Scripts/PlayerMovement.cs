5a699cba94d771ed47abeb37e00c649f424843c6
SickScottMondo <sickscottmondo@gmail.com>
2014-07-23 16:52:52 -0400

    First Commit
    First Commit of Cubey, to get the game up and viewable by Chris and
    Scott

---
+++ Cube Fun/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float moveSpeed;
+	public GameObject deathParticles;
+
+	private float maxSpeed = 5f;
+	private Vector3 input;
+
+	private Vector3 spawn;
+	// Use this for initialization
+	void Start () 
+	{
+		spawn = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		if (rigidbody.velocity.magnitude < maxSpeed)
+		{
+			rigidbody.AddForce (input * moveSpeed);
+		}
+
+		if (transform.position.y < -2) 
+		{
+			Die ();
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.transform.tag == "Enemy")
+		{
+			Die();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Goal")
+		{
+			GameManager.CompleteLevel();
+		}
+	}
+
+	void Die()
+	{
+		Instantiate(deathParticles, transform.position, Quaternion.identity);
+		transform.position = spawn;
+	}
+
+}
