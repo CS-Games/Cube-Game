b8f3315dc2e53ac6d772ddc646fce5b3f8ede167
Chris Vansco <chris.vansco@gmail.com>
2014-08-07 11:37:13 -0400

    new movement

--- Cube Fun/Assets/Scripts/PlayerMovement.cs
+++ Cube Fun/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,13 @@ public class PlayerMovement : MonoBehaviour
 	public float moveSpeed;
 	public GameObject deathParticles;
 	public float jumpHeight = 2;
-	private bool onGround; //need to figure out how to implement so you can't just keep pressing the spacebar
-
-	private float maxSpeed = 5f;
+	private bool onGround;
+	private bool touchWall;
 	private Vector3 input;
 
+	private float maxSpeed = 5f;
 	private Vector3 spawn;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -28,13 +29,21 @@ public class PlayerMovement : MonoBehaviour
 			rigidbody.AddForce(input * moveSpeed);
 		}
 
+		// Amount to Move
+//		float moveForward = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+//		float moveSideways = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;		
+//		
+//		// Move the player
+//		transform.Translate(Vector3.forward * moveForward);
+//		transform.Translate(Vector3.right * moveSideways);
+
 		if (transform.position.y < -2) 
 		{
 			Die ();
 		}
 
 		// Initiating the jump
-		if (Input.GetKeyDown(KeyCode.Space) && onGround) 
+		if (Input.GetKeyDown(KeyCode.Space) && onGround && !touchWall) 
 		{
 			gameObject.rigidbody.velocity += new Vector3(0,jumpHeight,0);
 			onGround = false;
@@ -51,6 +60,10 @@ public class PlayerMovement : MonoBehaviour
 		if (other.transform.tag == "Ground") 
 		{
 			onGround = true;
+		}
+		if (other.transform.tag == "Wall") 
+		{
+			touchWall = true;
 		}
 	}
 
